22e8c246027ea22e22bac826f5f93bbdb772ef22
ShadowCube264 <68464043+ShadowCube264@users.noreply.github.com>
2025-08-26 12:49:30 +0100

    Remove some logging (there may have been a failed attempt at JEI mixins, there's likely an easier way)

--- src/main/java/io/github/shadowcube264/createtweaks/CreateTweaks.java
+++ src/main/java/io/github/shadowcube264/createtweaks/CreateTweaks.java
@@ -3,7 +3,6 @@ package io.github.shadowcube264.createtweaks;
 import com.simibubi.create.Create;
 
 import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
-import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.resources.ResourceLocation;
@@ -20,10 +19,6 @@ public class CreateTweaks implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Create addon mod [{}] is loading alongside Create [{}]!", NAME, Create.VERSION);
-		LOGGER.info(EnvExecutor.unsafeRunForDist(
-				() -> () -> "{} is accessing Porting Lib from the client!",
-				() -> () -> "{} is accessing Porting Lib from the server!"
-		), NAME);
 		ForgeConfigRegistry.INSTANCE.register(ID, ModConfig.Type.COMMON, CreateTweaksConfig.CONFIG_SPEC);
 	}
 
